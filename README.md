f8115b8c7008893e073255d31e001c3737c4504c
Patrick Gallagher <patrickj@cpgallagher.com>
2019-02-15 02:46:55 -0600

    Initial firefox code

---
+++ README.md
@@ -0,0 +1,27 @@
+# ClassTranscribe Extension
+
+Long story short: there are some features we'd love to see in ClassTranscribe, but making changes
+upstream was too much work (for me, at least) at this time. This is a nice temporary solution for
+those of use who watch lectures on the site, and want things like remembering your position in the
+playlist, and autoplay!
+
+What's in the box:
+ - A Firefox extension
+ - (Soon TM) A Chrome extension
+
+Contributions are _more_ than welcome, including code cleanup, feature additions, or bug fixes.
+
+# Download
+If you want to just get the extension, head to the [Releases](https://github.com/pattyjogal/classtranscrbe-ext)
+page. For Firefox, you can just run the `.xpi` file, and it should add to your browser with no
+problem. Chrome instructions coming soon!
+
+# Features
+ - When you play a video, the site will remember it and when you load the site again, that video
+ will be auto selected. This helps you keep your last spot, and saves you a bit of time.
+
+# Coming Soon
+ - Chrome Extension (should be an easy port)
+ - Autoplay
+
+Please let me know in a Github Issue if you have any suggestions!
---
+++ firefox/src/ct_extensions.js
@@ -0,0 +1,33 @@
+document.body.style.border = "5px solid red";
+// On page load, change the video to be the last
+// watched one.
+$(document).ready(() => {
+    console.log('begin')
+    let index = browser.storage.local.get('ct_last_watched_index')
+        .then(
+            (index) => {
+                // We got the index successfuly.
+                // We load it, and click on it.
+                console.log(index);
+                index = index['ct_last_watched_index'];
+                console.log($('.vjs-playlist-item-list'))
+                $('.vjs-playlist-item-list').children().eq(index).click();
+            },
+            (error) => {
+                // We could not get the index
+                console.error(error);
+            }
+        );
+});
+// Add a listener for when a video is played
+var videos = document.querySelectorAll('video');
+
+Array.from(videos).forEach(video => {
+    video.addEventListener('play', e => {
+        console.log("PLAY");
+        let index = $('.vjs-playlist-item.vjs-selected').index();
+        browser.storage.local.set({
+            'ct_last_watched_index': index
+        });
+    })
+})
